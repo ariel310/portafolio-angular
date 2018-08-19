673e76191ff5e21ba74ce3be6cee97236391cd05
Ariel Nadal <ariel310_rc@hotmail.com>
2018-08-19 11:03:09 -0300

    Fin Seccion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
@@ -17,11 +17,12 @@ import { ItemComponent } from './pages/item/item.component';
     FooterComponent,
     PortafolioComponent,
     AboutComponent,
-    ItemComponent
+    ItemComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,13 @@
+
+
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  name?: string;
+  autor?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  tumblr?: string;
+  workTeam?: any[];
+}
