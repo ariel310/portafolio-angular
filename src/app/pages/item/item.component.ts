ef0afbd83cc4bb24077b956cb015dd7f1ff772bc
Ariel Nadal <ariel310_rc@hotmail.com>
2018-08-25 18:49:07 -0300

    Fin seccion 7

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Routes, ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
+import { ProductoDescripcion } from '../../interfaces/producto-descripcion.interface';
+import { InfoPaginaService } from '../../services/info-pagina.service';
 
 @Component({
   selector: 'app-item',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
-
-  constructor() { }
+  producto: ProductoDescripcion;
+  id: string;
+  constructor( private Route: ActivatedRoute,
+                public productoService: ProductosService,
+                public pagina: InfoPaginaService) { }
 
   ngOnInit() {
+
+    this.Route.params
+        .subscribe( parametros => {
+
+         // console.log(parametros['id']);
+          this.productoService.getProducto(parametros['id'])
+              .subscribe( (producto: ProductoDescripcion) => {
+                this.id = parametros['id'];
+                this.producto = producto;
+
+              });
+        });
   }
 
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
+import { resolve } from 'path';
+import { reject } from 'q';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,7 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrados: Producto[] = [];
 
   constructor( private http: HttpClient) {
     this.cargarProductos();
@@ -16,11 +19,47 @@ export class ProductosService {
   }
 
   private cargarProductos() {
-      this.http.get('https://angular-html-196db.firebaseio.com/productos_idx.json')
+      return new Promise( (resolve, reject ) => {
+        this.http.get('https://angular-html-196db.firebaseio.com/productos_idx.json')
         .subscribe(( resp: Producto[] ) => {
-            console.log(resp);
             this.cargando = false;
             this.productos = resp;
+            resolve();
         });
+
+
+      });
+
+  }
+
+  getProducto( id: string ) {
+      return this.http.get(`https://angular-html-196db.firebaseio.com/productos/${ id }.json`);
+
+  }
+
+  buscarProducto( termino: string) {
+
+    if (this.productos.length === 0) {
+        this.cargarProductos().then( () => {
+          this.filtrarProductos(termino);
+
+        });
+    } else {
+      this.filtrarProductos(termino);
+    }
+
+  }
+
+  private filtrarProductos( termino: string) {
+    this.productosFiltrados = [];
+    termino = termino.toLowerCase();
+    this.productos.forEach( prod => {
+      const tituloLower = prod.titulo.toLowerCase();
+      const categoriaLower = prod.categoria.toLowerCase();
+      if ( categoriaLower.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0) {
+        this.productosFiltrados.push( prod );
+      }
+
+    });
   }
 }
